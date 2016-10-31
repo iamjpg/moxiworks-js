fe8f0cbb32ae23f56a0b7a78afe1720b596428cc
JP Given <jgiven@gmail.com>
2016-10-30 20:51:08 -0700

    ...

--- test/foo_test.js
+++ test/foo_test.js
@@ -1,10 +1,24 @@
 import { expect } from 'chai';
 import { Foo } from '../src/foo';
+import 'whatwg-fetch';
 
-describe('Foo', () => {
-  it('should be runing without any problems', () => {
+describe('Foo.bar', () => {
+  it('better be string, yo', () => {
     const foo = new Foo;
     expect(foo.bar()).to.be.a('string');
+  });
+});
+
+describe('Foo.baz', () => {
+  it('should === 3', () => {
+    const foo = new Foo;
     expect(foo.baz()).equal(3);
+  });
+});
+
+describe('Foo.fetchListingData', () => {
+  it('should return an object', () => {
+    const foo = new Foo;
+    expect(foo.fetchListingData()).to.be.an('object');
   });
 });
