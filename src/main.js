0e1ee51fd1a16eefacef9ab6a4dda8aae9111f74
JP Given <jgiven@gmail.com>
2016-10-31 12:39:27 -0700

    Checking in things before I really mess them up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// Import all of your classes here. Foo is an example. Be sure to import for
+// CommonJS as well as AMD Modules.
+
 // Import EVERYTHING in Foo so it can be imported by any commonJS supported
 // environment (NodeJS, Angular, Ember, React, VueJS, etc...)
 import * as Foo from './foo';
