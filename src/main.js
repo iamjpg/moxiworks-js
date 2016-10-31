54739dcab5b259ed52d9eb735172cb5a491fa681
JP Given <jgiven@gmail.com>
2016-10-31 16:09:32 -0700

    ...

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Defenition for client side modules NOT using CommonJS. Essentially this
-// supports AMD modules(requireJS).
+// supports AMD modules(requireJS, Almond, etc...).
 if ( typeof define === "function" && define.amd ) {
   define("Moxi", [], function() {
     return {
