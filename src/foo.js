b59ba989f7d6af69824912badff0e61a6edb2f28
JP Given <jgiven@gmail.com>
2016-11-01 09:30:09 -0700

    ...

--- src/foo.js
+++ src/foo.js
@@ -3,11 +3,21 @@ export class Foo {
     this.name = name;
   }
 
-  bar() {
-    return 'this is bar!';
+  /**
+   * This function returns listings based on a geographic region.
+   * @param {string} is a string.
+   * @returns {string} phrase appending string.
+   */
+  bar(str='bar') {
+    return `this is ${str}`;
   }
 
-  baz() {
-    return 3;
+  /**
+   * This function returns listings based on a geographic region.
+   * @param {number} number to return.
+   * @returns {number} new number.
+   */
+  baz(n=3) {
+    return n;
   }
 }
--- src/z-amd.js
+++ src/z-amd.js
@@ -1,5 +1,10 @@
-// Defenition for client side modules NOT using CommonJS. Essentially this
-// supports AMD modules(requireJS, Almond, etc...).
+/*
+ * Defenition for client side modules NOT using CommonJS. Essentially this
+ * supports AMD modules(requireJS, Almond, etc...).
+ *
+ * Unlike CommonJS, this will need to be updated manually as Class definitions
+ * are made.
+ */
 if ( typeof define === "function" && define.amd ) {
   define("Moxi", [], function() {
     return {
